0307491f9c73694d0ccf382ca24bed783e8f50fe
Simon Paul <samonpaul06@gmail.com>
2024-08-28 10:53:18 +0530

    feat: added sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,16 @@ const Sidebar = () => {
     <div className='sidebar'>
         <div className="sidebar-header">
             <div className="channel-logo"></div>
+            <h3 className='sidebar-primary'>channel name</h3>
+            <p className='sidebar-uid'>@user_id</p>
+        </div>
+        <div className="sidebar-nav">
+            <p className='side-nav active'>Dashboard</p>
+            <p className='side-nav'>Content</p>
+            <p className='side-nav'>Analytics</p>
+            <p className='side-nav'>Earn</p>
+            <p className='side-nav'>Settings</p>
+            <p className='side-nav'>Send feedback</p>
         </div>
     </div>
   )
